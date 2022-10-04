1fbe5634c52cfbd6ed57db6bcf4603c773525269
Wu Tingfeng <wutingfeng@outlook.com>
2022-10-04 17:14:04 +0800

    add test cases for opToString

--- xnotify/xnotify_test.go
+++ xnotify/xnotify_test.go
@@ -1,6 +1,39 @@
 package xnotify
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func TestOpToString(t *testing.T) {
+	type opToStringTest struct {
+		opCode   fsnotify.Op
+		expected string
+	}
+	testCases := []opToStringTest{
+		{fsnotify.Create, "create"},
+		{fsnotify.Write, "write"},
+		{fsnotify.Remove, "remove"},
+		{fsnotify.Rename, "rename"},
+		{fsnotify.Chmod, "chmod"},
+	}
+	for _, testCase := range testCases {
+		if res := opToString(testCase.opCode); res != testCase.expected {
+			t.Errorf("Expected %q, got %q", testCase.expected, res)
+		}
+	}
+}
+
+func TestOpToStringInvalid(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic for invalid fsnotify op code")
+		}
+	}()
+	var invalidOp fsnotify.Op = 999
+	opToString(invalidOp)
+}
 
 func TestFullURL(t *testing.T) {
 	type fullURLTest struct {
